a811d08ed6e093f0a20bede017b63b75eb8d5324
YanniAlshoufiHTL <114535252+YanniAlshoufiHTL@users.noreply.github.com>
2023-01-28 23:17:36 +0100

    Made Nav Unanimous and Added Homepage Link to Logo

--- js/nav.js
+++ js/nav.js
@@ -2,11 +2,12 @@ class NavBar extends HTMLElement {
 	constructor() {
 		super();
 		this.innerHTML = `<nav>
+    <link rel="stylesheet" href="/global-stylings/css/nav-styling.css" />
     <div class="wrapper">
-      <div id="logo-div">.mohay</div>
+      <a id="logo-link" href="/">.mohay</a>
       <div id="nav-div">
-        <a href="pages/documentation.html">Documentation</a>
-        <a href="pages/about-us.html">About Us</a>
+        <a href="/pages/documentation.html">Documentation</a>
+        <a href="/pages/about-us.html">About Us</a>
         <div id="nav-btn-div">
           <button id="nav-codespace-btn" class="codespace-btn">Codespace</button>
         </div>
