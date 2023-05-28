be588ec218da09a7fc3d090d360bd56eea5912d0
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-05-28 15:17:59 +0200

    update

--- backend/Analyzer/Analyzer/Analyzer.js
+++ backend/Analyzer/Analyzer/Analyzer.js
@@ -1,29 +1,8 @@
-function analyze(filename) {
-    let code = document.getElementById("myDiv").innerText;
+function Analyze(code){
+    var errors = [];
+    var warnings = [];
 
-    let erros = [];
-    let tokens = [];
+    var 
 
-    let lines = code.split("\n");
-    lines = lines.filter((item) => item !== "");
 
-    for (let i = 0; i < lines.length; i++) {
-        let line = lines[i];
-        line = deleteCharBetween(line, "(", ")", " ");
-        let lineTokens = line.split(" ");
-        lineTokens = lineTokens.filter((item) => item !== "");
-
-        let linetokens = [];
-        for (let j = 0; j < lineTokens.length; j++) {
-            let token = lineTokens[j];
-            let tokenType = getTokenType(token);
-            if (tokenType === "error") {
-                erros.push(token);
-            }
-            linetokens.push({ token, tokenType });
-        }
-        let lineResult = checkline(linetokens);
-        tokens.push([linetokens, lineResult]);
-    }
-    console.log(tokens);
 }
