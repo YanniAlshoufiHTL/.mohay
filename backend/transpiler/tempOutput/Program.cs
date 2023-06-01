f217e5a7d34f28349d6d3a7c37f0fbef1c58a410
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-06-01 12:50:09 +0200

    well fuck

--- backend/transpiler/tempOutput/Program.cs
+++ backend/transpiler/tempOutput/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace tempOutput;
+
+class ZuWild {
+    static void Main(string[] args) {
+
+    }
+}
