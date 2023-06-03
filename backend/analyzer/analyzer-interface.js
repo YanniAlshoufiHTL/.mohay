050dc0556eac28af0dd3c480112a8e9ee5b657c7
YanniAlshoufiHTL <114535252+YanniAlshoufiHTL@users.noreply.github.com>
2023-06-04 00:38:42 +0200

    Made sure modules are setup correctly and almost finished the method that returns the syntax definition of the keyword used

--- backend/analyzer/analyzer-interface.js
+++ backend/analyzer/analyzer-interface.js
@@ -1,21 +1,99 @@
+import { languageSyntax } from "./analyzer-syntax.js";
+
+/*
+ * analyze function (takes String, outputs object)
+ *
+ * output object interface:
+ * {
+ *  codeCorrect: boolean,
+ *
+ *  keywords: […], // example for keyword: { value: "wow", row: 4, col: 5 }
+ *  expressions: […], // example for expression: { occurence: "PI", row: 1, col: 0 }
+ *
+ *  variables: […], // example for variable: { name: "peter", row: 22, col: 10 }
+ *  constants: […], // example for constant: { name: "PI", row: 1, col: 0 }
+ *  functions: […], // example for function: { name: "rect", row: 50, col: 10 }
+ *
+ *  errors: […], // example for error: { message: "Unknown symbol 'f'.", row: 3, col_start: 10, col_end: 15 }
+ * }
+ *
+ */
+
 function analyze(code) {
-  let result = {};
+  let result = {
+    codeCorrect: false,
+    
+    keywords: [],
+    expressions: [],
 
-  let lines = code.split("\n");
+    variables: [],
+    constants: [],
+    functions: [],
 
-  for (let line in lines) {
+    errors: [],
+  };
+
+  let lines = code.split(/\r?\n/);
+
+  for (let line of lines) {
+    if (line[0] === "$")
+      return "TODO: MAKE THIS RETURN ERROR!";
+
     console.log(line);
   }
 
   return result;
 }
 
+/* example result: { ["inline": ["//", "// <text>", "//", "<text>"] }
+ * if multiple lines are affected, the following is a valid response using "$nextLine"
+ * { "$nextLine": 10, ["/*", "/* <mtext> *\/", "/*", "<ml>", "<text>", "<ml>", "*\/"] }
+ *
+ * if no result was found, the function returns undefined
+ */
 
-analyze(`
-Hello there!
+/*
+ let bar = undefined;
 
-My name is Y
+  for (const [index, [innerKey, innerValue]] of Object.entries(Object.entries(languageSyntax[key]))) {
+    if (Object.entries(languageSyntax[key])[index][0] === "$common" && +index === 0) {
+      bar = innerValue;
+    } else {
 
-anni.
+    }
+  }
+*/
 
-`);
+function getSyntaxObjectLine(line) {
+  let result = {}
+
+  for (const key in languageSyntax) {
+    for (const [index, [innerKey, innerValue]] of Object.entries(Object.entries(languageSyntax[key]))) {
+
+      let keyword = innerValue[0];
+      if (keyword && keyword !== "" && innerKey[0] !== "$") {
+        let firstWordOfLine = line.split(" ")[0];
+        //
+        // console.log("\n" + firstWordOfLine);
+        // console.log(keyword);
+        //
+        // for (let i = 0; i < keyword.length && firstWordOfLine.length; i++) {
+        //   console.log(keyword.codePointAt(i), firstWordOfLine.codePointAt(i));
+        // }
+        //
+        // console.log(firstWordOfLine === keyword);
+
+        if (line.split(" ")[0] === keyword) {
+          console.log("PINGO!");
+          console.table("Keyword: " + keyword)
+          console.table(Object.entries(languageSyntax[key])[0]);
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+
+getSyntaxObjectLine('wow YANNI = "Yanni"');
--- backend/analyzer/analyzer-syntax.js
+++ backend/analyzer/analyzer-syntax.js
@@ -8,41 +8,44 @@
  * <mtext> : Plain multiline text
  * <text>  : Plain inline text
  *
- * Syntax is always given as an array, first containing the entire syntax and then the single elements of it. Spaces before and after the above named special conventions are ignored.
+ * Syntax is always given as an array, first containing the keyword or undefined for no keyword, then the entire syntax and lastly the single elements of it. Spaces before and after the above named special conventions are ignored.
  *
  * If a grouping (a js object) of things contains a partially equal syntax, the partially equal syntax is given at the beginning of the object with the "$common" key and each other element replaces it with a '|' symbol. Anything that IS different is denoted through '<param>' which will be replaced by the key of the object value.
  *
  * If spaces before or after '<…>' denoted syntax should NOT be ignored, a '<' + amout of spaces as space chars in js + '>' must be written.
  *
  * If there's an '*' before the '>' in a '<…>', the number of '<…>' given is up to the user.
+ *
  */
 
-let languageSyntax = {
+// TODO: MAKE SURE RETURN TYPE OF EXPRESSIONS IS SOMEHOW PROGRAMMED IN!!
+
+export let languageSyntax = {
   declarations: {
-    "constants": ["wow <name> = <Expr>", "<name>", "=", "<expr>"],
-    "variables": [".<><name> = <Expr>"],
+    "constants": ["wow" , "wow <name> = <expr>", "<name>", "=", "<expr>"],
+    "variables": [".", ".<><name> = <expr>", ".<><name>", "=", "<expr>"],
   },
 
   predefinedFunctions: {
     "$common": "<param>",
-    "rect": ["| <point> <nume> <nume>", "|", "<nume>", "<nume>"],
-    "circle": ["| <point> <nume>", "|", "<point>", "<nume>"],
-    "line": ["| <point> <point>", "|", "<point>", "<point>"],
-    "arc": ["| <point> <nume> <nume> <nume>", "|", "<nume>", "<nume>", "<nume>"],
-    "triangle": ["| <point> <point> <point>", "|", "<point>", "<point>", "<point>"],
-    "polygon": ["| <point> <nume> <nume> <nume>", "|", "<point>", "<nume>", "<nume>", "<nume>"],
-    "vector": ["| <point*>", "|", '<point*>'],
+    "rect": ["rect", "| <point> <nume> <nume>", "|", "<point>", "<nume>", "<nume>"],
+    "circle": ["circle", "| <point> <nume>", "|", "<point>", "<nume>"],
+    "line": ["line", "| <point> <point>", "|", "<point>", "<point>"],
+    "arc": ["arc", "| <point> <nume> <nume> <nume>", "|", "<nume>", "<nume>", "<nume>"],
+    "triangle": ["triangle", "| <point> <point> <point>", "|", "<point>", "<point>", "<point>"],
+    "polygon": ["polygon", "| <point> <nume> <nume> <nume>", "|", "<point>", "<nume>", "<nume>", "<nume>"],
+    "vector": ["vector", "| <point*>", "|", '<point*>'],
   },
 
   predefinedConstants: {
     "$common": "<param>",
-    "PI": ["|", "|"],
-    "E": ["|", "|"],
+    "PI": [undefined, "|", "|"],
+    "E": [undefined, "|", "|"],
   },
 
   comments: {
-    "inline": ["// <text>", "//", "<text>"],
-    "multiline": ["/* <mtext> */", "/*", "<ml>", "<text>", "<ml>", "*/"],
+    "inline": ["//", "// <text>", "//", "<text>"],
+    "multiline": ["/*", "/* <mtext> */", "/*", "<ml>", "<text>", "<ml>", "*/"],
   }
 }
 
