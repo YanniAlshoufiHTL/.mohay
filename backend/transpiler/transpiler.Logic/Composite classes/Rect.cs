269880e21c043100598338441736a2a512ff3f30
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-06-06 15:10:55 +0200

    add new transpiler

---
+++ backend/transpiler/transpiler.Logic/Composite classes/Rect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using transpiler.Logic.Composite_interfaces;
+
+namespace transpiler.Logic.Composite_classes {
+    internal class Rect : IKeyword {
+        public IString? Name { get; set; }
+        public IPosition? Pos { get; set; }
+        public INumeric? NumX { get; set; }
+        public INumeric? NumY { get; set; }
+    }
+}
