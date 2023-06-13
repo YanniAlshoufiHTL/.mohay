6bd54b58bedf4bacbc3ac978b21f05be951288e6
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-06-13 13:58:59 +0200

    Update Codespace.vue

--- frontend/src/views/Codespace.vue
+++ frontend/src/views/Codespace.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="codespace-wrapper">
-    <div id="top-bar-wrapper">
-      <div id="menu-wrapper">
-        <div class="menu-element">File</div>
-        <div class="menu-element">Edit</div>
-        <div class="menu-element">Find</div>
-        <div class="menu-element">Navigate</div>
-        <div class="menu-element">Project</div>
-        <div class="menu-element">Help</div>
-      </div>
-      <div id="profile-icon-wrapper">
-        <div class="profile-icon"></div>
-      </div>
+    <div id="codespace-wrapper">
+        <div id="top-bar-wrapper">
+            <div id="menu-wrapper">
+                <div class="menu-element">File</div>
+                <div class="menu-element">Edit</div>
+                <div class="menu-element">Find</div>
+                <div class="menu-element">Navigate</div>
+                <div class="menu-element">Project</div>
+                <div class="menu-element">Help</div>
+            </div>
+            <div id="profile-icon-wrapper">
+                <div class="profile-icon"></div>
+            </div>
+        </div>
+        <div id="main-wrapper">
+            <div id="Preview"></div>
+            <div id="folder-viewer"></div>
+            <div id="code-area"></div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Codespace',
+    name: "Codespace",
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../styles/global.scss';
-@import '../styles/variables.scss';
+@import "../styles/global.scss";
+@import "../styles/variables.scss";
 
 $select-ui-color: #77769642;
 $top-bar-height: 50px;
@@ -34,131 +39,147 @@ $bar-element-border-weight: 4px;
 
 $main-wrapper-width: 95vw;
 
-#codespace-wrapper {
-  position: relative;
-  left: calc((100vw - $main-wrapper-width) / 2);
+#main-wrapper {
+    position: absolute;
+    top: calc(calc(var(--nav-height) / 10) + 7vh);
+    background-color: var(--bg-color);
+    border: var(--codespace-box-border-color);
+    border-width: 0.2rem;
+    border-style: solid;
 
-  width: $main-wrapper-width;
+    width: 95vw;
+    height: 74vh;
+    border-radius: 0.5rem;
+}
+
+#codespace-wrapper {
+    position: relative;
+    left: calc((100vw - $main-wrapper-width) / 2);
+
+    width: $main-wrapper-width;
 }
 
 #top-bar-wrapper {
-  position: relative;
-  top: calc(var(--nav-height) / 10);
+    position: relative;
+    top: calc(var(--nav-height) / 10);
 
-  display: flex;
-  width: 100%;
-  height: $top-bar-height;
-
-  background-color: var(--bg-color);
-  font-family: var(--primary-font);
-
-  #menu-wrapper {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    width: 100%;
+    height: $top-bar-height;
 
-    width: $top-bar-elements-width;
-    box-sizing: border-box;
+    background-color: var(--bg-color);
+    font-family: var(--primary-font);
 
-    font-size: 1.2rem;
+    #menu-wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
 
-    color: var(--codespace-menu-bar-fg-color);
+        width: $top-bar-elements-width;
+        box-sizing: border-box;
 
-    .menu-element {
-      min-width: 110px;
-      text-align: center;
-      font-weight: bold;
+        font-size: 1.2rem;
 
-      padding: 3px 12px;
+        color: var(--codespace-menu-bar-fg-color);
 
-      user-select: none;
-      box-sizing: border-box;
+        .menu-element {
+            min-width: 110px;
+            text-align: center;
+            font-weight: bold;
 
-      border: $bar-element-border-weight solid var(--codespace-menu-btn-border-color);
-      border-radius: 13px;
+            padding: 3px 12px;
 
-      background-color: var(--codespace-menu-btn-bg-color);
-      transition: background filter 0.1s;
-      cursor: pointer;
+            user-select: none;
+            box-sizing: border-box;
 
-      &:hover {
-        background-color: var(--codespace-menu-btn-bg-hover-color);
-      }
+            border: $bar-element-border-weight solid
+                var(--codespace-menu-btn-border-color);
+            border-radius: 13px;
 
-      &:active {
-        filter: brightness(0.8);
-      }
+            background-color: var(--codespace-menu-btn-bg-color);
+            transition: background filter 0.1s;
+            cursor: pointer;
+
+            &:hover {
+                background-color: var(--codespace-menu-btn-bg-hover-color);
+            }
+
+            &:active {
+                filter: brightness(0.8);
+            }
+        }
     }
-  }
 
-  #profile-icon-wrapper {
-    width: calc(100% - $top-bar-elements-width);
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
+    #profile-icon-wrapper {
+        width: calc(100% - $top-bar-elements-width);
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
 
-    .profile-icon {
-      box-sizing: border-box;
-      background-color: var(--codespace-account-circle-bg-color);
+        .profile-icon {
+            box-sizing: border-box;
+            background-color: var(--codespace-account-circle-bg-color);
 
-      border-radius: 50%;
-      border: $bar-element-border-weight solid var(--codespace-account-circle-border-color);
-      height: calc($top-bar-height * 0.9);
-      width: calc($top-bar-height * 0.9);
+            border-radius: 50%;
+            border: $bar-element-border-weight solid
+                var(--codespace-account-circle-border-color);
+            height: calc($top-bar-height * 0.9);
+            width: calc($top-bar-height * 0.9);
+        }
     }
-  }
 }
 
 $frame-gap: 30px;
 #main-editor-frame {
-  position: fixed;
-  bottom: calc((100vw - $main-wrapper-width) / 2);
-
-  display: flex;
-  gap: $frame-gap;
-
-  height: calc(
-    100vh - var(--nav-height) - $top-bar-height - (100vw - $main-wrapper-width) / 2 -
-      calc(var(--nav-height) / 10) - (100vw - $main-wrapper-width) / 4
-  );
-  width: $main-wrapper-width;
-
-  background-color: var(--bg-color);
-
-  #non-editor-frame-section {
-    flex: 1;
+    position: fixed;
+    bottom: calc((100vw - $main-wrapper-width) / 2);
 
     display: flex;
-    flex-direction: column;
     gap: $frame-gap;
 
-    > * {
-      width: 100%;
-      background-color: var(--nav-bg);
+    height: calc(
+        100vh - var(--nav-height) - $top-bar-height -
+            (100vw - $main-wrapper-width) / 2 - calc(var(--nav-height) / 10) -
+            (100vw - $main-wrapper-width) / 4
+    );
+    width: $main-wrapper-width;
+
+    background-color: var(--bg-color);
+
+    #non-editor-frame-section {
+        flex: 1;
+
+        display: flex;
+        flex-direction: column;
+        gap: $frame-gap;
+
+        > * {
+            width: 100%;
+            background-color: var(--nav-bg);
+        }
+
+        #frame-run-window {
+            flex: 3;
+        }
+
+        #frame-explorer-window {
+            flex: 2;
+        }
     }
 
-    #frame-run-window {
-      flex: 3;
+    #frame-editor-section {
+        flex: 1;
+
+        display: flex;
+
+        #frame-editor {
+            flex: 1;
+            background-color: var(--nav-bg);
+
+            font-family: monospace;
+            font-size: 1rem;
+            padding: 30px;
+        }
     }
-
-    #frame-explorer-window {
-      flex: 2;
-    }
-  }
-
-  #frame-editor-section {
-    flex: 1;
-
-    display: flex;
-
-    #frame-editor {
-      flex: 1;
-      background-color: var(--nav-bg);
-
-      font-family: monospace;
-      font-size: 1rem;
-      padding: 30px;
-    }
-  }
 }
 </style>
